f5e698ec1d25ca1cd45d1a305bc5c701b61d330a
Seif-Elkasrawy <Seifeldin.kasrawy@hotmail.com>
2023-08-27 10:56:00 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { BlogComponent } from './blog/blog.component';
+import { ForumsComponent } from './forums/forums.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "" , component : HomeComponent},
+  {path : "games" , component : GamesComponent},
+  {path : "blog" , component : BlogComponent},
+  {path : "forums" , component : ForumsComponent},
+  {path : "contact" , component : ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { BlogComponent } from './blog/blog.component';
+import { ForumsComponent } from './forums/forums.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    GamesComponent,
+    BlogComponent,
+    ForumsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
