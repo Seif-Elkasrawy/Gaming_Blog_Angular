2b81a865d900221d5e1e75db39019a1b28f6a48f
Seif-Elkasrawy <Seifeldin.kasrawy@hotmail.com>
2023-08-27 15:41:44 +0200

    Update gaming-news.service.ts

--- src/app/gaming-news.service.ts
+++ src/app/gaming-news.service.ts
@@ -6,11 +6,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GamingNewsService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { 
+    
+  }
 
   getGamingNews(): any {
     return this.httpClient.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=d9fa182ee9ef44a5a756220c5bcb5a32')
   }
+
+  
 
 }
 
