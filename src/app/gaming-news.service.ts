c1c157963f86613912b94f120facfc2ed3a2f44f
Seif-Elkasrawy <Seifeldin.kasrawy@hotmail.com>
2023-08-28 18:35:13 +0200

    second

--- src/app/gaming-news.service.ts
+++ src/app/gaming-news.service.ts
@@ -11,8 +11,8 @@ export class GamingNewsService {
   }
 
   getGamingNews(): any {
-    //return this.httpClient.get('https://api.rawg.io/api/genres?key=9cff31644d4545daa1e416c3a784e879')
-    return this.httpClient.get('https://free-epic-games.p.rapidapi.com/free?X-RapidAPI-Key=48bdbc3571msh94ef5f4cb76590cp148103jsn88e9ba0ca5d7')
+    return this.httpClient.get('https://api.rawg.io/api/genres?key=9cff31644d4545daa1e416c3a784e879')
+    //return this.httpClient.get('https://free-epic-games.p.rapidapi.com/free?X-RapidAPI-Key=48bdbc3571msh94ef5f4cb76590cp148103jsn88e9ba0ca5d7')
   }
 
   
